ec97e987b8627e4fce80789d057ac1a40981f782
Eduardo Yañez <eduardo_yanez@live.com.mx>
2023-11-03 22:37:26 -0600

    Update telegramSendScript.js

--- telegramSendScript.js
+++ telegramSendScript.js
@@ -1,21 +1,20 @@
 async function enviarScript(scriptText){
 	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
-	main = document.querySelector(`div.chat-input-main`),
-	textarea = main.querySelector(`div.input-message-input`)
+	main = document.querySelector("#page-chats")
+	textarea = main.querySelector(`div[contenteditable="true"]`)
 	
 	if(!textarea) throw new Error("Não há uma conversa aberta")
 	
 	for(const line of lines){
 		console.log(line)
 
-		main.focus();
 		textarea.focus();
 		document.execCommand('insertText', false, line);
 		textarea.dispatchEvent(new Event('change', {bubbles: true}));
 	
 		setTimeout(() => {
 			(main.querySelector(`button.send`)).click();
-		}, 200);
+		}, 100);
 		
 		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
 	}
@@ -26,4 +25,3 @@ async function enviarScript(scriptText){
 enviarScript(`
 Joder en Telegram
 `).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
-
